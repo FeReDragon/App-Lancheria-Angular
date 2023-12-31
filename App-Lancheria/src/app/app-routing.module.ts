4aa3c209537cb7b040e58135e6808a66b52505f1
“FeReDragon” <f3030ma@gmail.com>
2023-12-31 11:05:54 -0300

    update rotas

--- App-Lancheria/src/app/app-routing.module.ts
+++ App-Lancheria/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './shared/components/home/home.component'; // Importar HomeComponent
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
+import { ProductListComponent } from './modules/catalog/components/product-list/product-list.component';
 
 
 const routes: Routes = [
@@ -10,6 +11,7 @@ const routes: Routes = [
   { path: 'home', component: HomeComponent }, // Rota para HomeComponent
   { path: 'login', component: LoginComponent },
   { path: 'registro', component: RegisterComponent },
+  { path: 'menu', component: ProductListComponent },
   // Outras rotas podem ser adicionadas aqui
 ];
 
