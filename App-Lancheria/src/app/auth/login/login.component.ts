254a091e0346aa75bb64eed624b9e8c0ea0b0037
“FeReDragon” <f3030ma@gmail.com>
2023-12-31 11:03:18 -0300

    update modulo autenticacao

--- App-Lancheria/src/app/auth/login/login.component.ts
+++ App-Lancheria/src/app/auth/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  email: string = '';
+  senha: string = '';
 
+
+  constructor(private http: HttpClient, private router: Router) {}
+
+  onLogin() {
+    this.http.get<any[]>('http://localhost:3000/usuarios?email=' + this.email + '&senha=' + this.senha)
+      .subscribe(usuarios => {
+        if (usuarios.length > 0) {
+          console.log('Login bem-sucedido', usuarios[0]);
+          this.router.navigate(['/home']); // Redireciona para a rota /home
+        } else {
+          console.error('Falha no login');
+          // Aqui você pode adicionar lógica para lidar com falhas no login
+        }
+      });
+  }
 }
