7cfbfc5ad6827220377ef2713158a24a89d87290
“FeReDragon” <f3030ma@gmail.com>
2024-01-01 10:16:30 -0300

    carrinho model

--- App-Lancheria/src/app/shared/model/cart.model.ts
+++ App-Lancheria/src/app/shared/model/cart.model.ts
@@ -7,6 +7,7 @@ export interface CartItem {
 }
 
 export interface Cart {
+    usuarioId: number; // Identificador do usuário ao qual o carrinho pertence
     itens: CartItem[];
     totalItens: number; // Número total de itens no carrinho
     totalPreco: number; // Soma total dos preços dos itens no carrinho
