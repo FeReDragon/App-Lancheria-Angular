93a6301077e067191c09f8d7045fb9b3a5c87428
“FeReDragon” <f3030ma@gmail.com>
2024-01-03 22:46:43 -0300

    update modulo admin

--- App-Lancheria/src/app/modules/admin/components/admin-dashboard/admin-dashboard.component.ts
+++ App-Lancheria/src/app/modules/admin/components/admin-dashboard/admin-dashboard.component.ts
@@ -1,10 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../../../core/services/product.service';
+import { OrdersService } from '../../../../core/services/orders-service.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-dashboard',
   templateUrl: './admin-dashboard.component.html',
   styleUrls: ['./admin-dashboard.component.scss']
 })
-export class AdminDashboardComponent {
+export class AdminDashboardComponent implements OnInit {
+  totalProdutos: number = 0;
+  totalPedidos: number = 0;
+  totalPedidosUltimoDia: number = 0;
+
+  constructor(
+    private productService: ProductService,
+    private ordersService: OrdersService, 
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    this.obterTotalProdutos();
+    this.obterTotalPedidos();
+    this.obterTotalPedidosUltimoDia();
+  }
+
+  obterTotalProdutos() {
+    this.productService.getProdutos().subscribe(
+      produtos => {
+        this.totalProdutos = produtos.length;
+      },
+      erro => {
+        console.error('Erro ao obter total de produtos', erro);
+      }
+    );
+  }
+  obterTotalPedidos() {
+    this.ordersService.getOrders().subscribe(
+      pedidos => {
+        this.totalPedidos = pedidos.length;
+      },
+      erro => {
+        console.error('Erro ao obter total de pedidos', erro);
+      }
+    );
+  }
+
+  obterTotalPedidosUltimoDia() {
+    const dataOntem = new Date();
+    dataOntem.setDate(dataOntem.getDate() - 1);
+
+    this.ordersService.getOrders().subscribe(
+      pedidos => {
+        this.totalPedidosUltimoDia = pedidos.filter(pedido => 
+          new Date(pedido.dataPedido) > dataOntem
+        ).length;
+      },
+      erro => {
+        console.error('Erro ao obter total de pedidos no último dia', erro);
+      }
+    );
+  }
+
+  // Métodos obterTotalPedidos() e obterTotalPedidosUltimoDia() permanecem iguais
+
+  navegarParaGerenciamentoProdutos() {
+    // Substitua pelo caminho correto
+    this.router.navigate(['/adm-itens']);
+  }
 
 }
--- App-Lancheria/src/app/modules/admin/components/order-management/order-management.component.ts
+++ App-Lancheria/src/app/modules/admin/components/order-management/order-management.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { OrdersService } from '../../../../core/services/orders-service.service';
+import { UserService } from '../../../../core/services/user.service';
+import { Order } from 'src/app/shared/model/order.model';
+import { User } from 'src/app/shared/model/user.model';
+import { forkJoin } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-order-management',
   templateUrl: './order-management.component.html',
   styleUrls: ['./order-management.component.scss']
 })
-export class OrderManagementComponent {
+export class OrderManagementComponent implements OnInit {
+  orders: any[] = []; // Use 'any[]' para incluir a propriedade 'userName'
+  users: User[] = [];
 
+  constructor(
+    private ordersService: OrdersService,
+    private userService: UserService
+  ) {}
+
+  ngOnInit() {
+    forkJoin({
+      orders: this.ordersService.getOrders(),
+      users: this.userService.getUsers()
+    }).pipe(
+      map(({ orders, users }) => {
+        this.users = users;
+        return orders.map(order => ({
+          ...order,
+          userName: users.find(user => user.id === order.usuarioId)?.nome || 'Desconhecido'
+        }));
+      })
+    ).subscribe(ordersWithUserName => {
+      this.orders = ordersWithUserName;
+    });
+  }
+  
+  atualizarStatus(orderId: number, novoStatus: string) {
+    const pedidoAtualizado: Partial<Order> = {
+      id: orderId,
+      status: novoStatus
+    };
+
+    this.ordersService.atualizarOrder(pedidoAtualizado).subscribe(() => {
+      // Atualização bem-sucedida
+    },
+    erro => {
+      console.error('Erro ao atualizar o pedido', erro);
+    });
+  }
 }
