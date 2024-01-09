bf35cd4b3a6192ba9374691f1eaa0f71147df610
“FeReDragon” <f3030ma@gmail.com>
2024-01-09 17:57:18 -0300

    update componente catalog

--- App-Lancheria/src/app/modules/catalog/catalog.module.ts
+++ App-Lancheria/src/app/modules/catalog/catalog.module.ts
@@ -3,20 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { CatalogRoutingModule } from './catalog-routing.module';
 import { CategoryListComponent } from './components/category-list/category-list.component';
-import { CategoryItemComponent } from './components/category-item/category-item.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
-import { InventoryManagementComponent } from './components/inventory-management/inventory-management.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 
 
 @NgModule({
   declarations: [
     CategoryListComponent,
-    CategoryItemComponent,
     ProductListComponent,
     ProductDetailsComponent,
-    InventoryManagementComponent
   ],
   imports: [
     CommonModule,
