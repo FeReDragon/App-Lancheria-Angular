355831fd22640d4c24268b8e4adbf174f8954814
“FeReDragon” <f3030ma@gmail.com>
2024-01-10 19:03:24 -0300

    update integracao com promocao

--- App-Lancheria/src/app/modules/catalog/components/product-list/product-list.component.ts
+++ App-Lancheria/src/app/modules/catalog/components/product-list/product-list.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Produto } from '../../../../shared/model/product.model';
 import { ProductService } from '../../../../core/services/product.service';
+import { PromotionService } from '../../../../core/services/promotion.service';
+import { Promotion } from '../../../../shared/model/promotion.model';
 import { Category } from '../../../../shared/model/category.model';
 import { CategoryService } from '../../../../core/services/category.service';
-import { CartService } from '../../../../core/services/cart.service'; // Importe o CartService
+import { CartService } from '../../../../core/services/cart.service';
 import { AuthService } from '../../../../core/services/auth.service';
 
 @Component({
@@ -14,22 +16,24 @@ import { AuthService } from '../../../../core/services/auth.service';
 export class ProductListComponent implements OnInit {
   categorias: Category[] = [];
   produtos: Produto[] = [];
+  promocoes: Promotion[] = []; 
   selectedCategoriaId?: number;
   produtoSelecionado?: Produto;
   quantidadeSelecionada: number = 1;
   selectedCategoriaNome?: string;
 
-
   constructor(
     private productService: ProductService,
+    private promotionService: PromotionService, 
     private categoryService: CategoryService,
-    private cartService: CartService, // Injete o CartService
+    private cartService: CartService,
     private authService: AuthService
   ) {}
 
   ngOnInit() {
     this.loadCategorias();
     this.loadProdutos();
+    this.loadPromocoes(); 
   }
 
   loadCategorias() {
@@ -40,31 +44,54 @@ export class ProductListComponent implements OnInit {
   }
 
   loadProdutos() {
-    if (this.selectedCategoriaNome) {
-      this.productService.getProdutosPorCategoriaNome(this.selectedCategoriaNome).subscribe(
-        data => this.produtos = data,
-        error => console.error(error)
-      );
-    } else {
-      this.productService.getProdutos().subscribe(
-        data => this.produtos = data,
-        error => console.error(error)
-      );
+    this.productService.getProdutos().subscribe(
+      data => {
+        this.produtos = data;
+        this.applyPromotionsToProducts();
+      },
+      error => console.error(error)
+    );
+  }
+
+  loadPromocoes() {
+    this.promotionService.getPromocoes().subscribe(
+      data => {
+        this.promocoes = data;
+        this.applyPromotionsToProducts(); // Aplicar promoções após carregar
+      },
+      error => console.error(error)
+    );
+  }
+
+  applyPromotionsToProducts() {
+    if (this.promocoes.length > 0 && this.produtos.length > 0) {
+      this.produtos = this.produtos.map(produto => {
+        return {
+          ...produto,
+          preco: this.calculateDiscountedPrice(produto)
+        };
+      });
     }
   }
-  
+
+  calculateDiscountedPrice(produto: Produto): number {
+    let discountedPrice = produto.preco;
+    this.promocoes.forEach(promocao => {
+      if (promocao.produtosAplicaveis && promocao.produtosAplicaveis.includes(produto.id)) {
+        discountedPrice *= (1 - promocao.desconto / 100);
+      }
+    });
+    return discountedPrice;
+  }
 
   onCategoriaSelecionada(categoriaNome: string) {
     this.selectedCategoriaNome = categoriaNome;
     this.loadProdutos();
   }
-  
-   
 
   adicionarAoCarrinho(produto: Produto) {
     const usuarioAtual = this.authService.currentUserValue;
     if (usuarioAtual && usuarioAtual.id) {
-      // Certifique-se de que o id é um número válido e não zero
       this.cartService.adicionarAoCarrinho(usuarioAtual.id, produto).subscribe(cart => {
         // Lógica de sucesso
       });
