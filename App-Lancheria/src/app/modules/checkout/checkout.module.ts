ab9f021536b2cb33e37916987029b79c83303c62
“FeReDragon” <f3030ma@gmail.com>
2024-01-21 18:08:21 -0300

    validacoes formulario de order

--- App-Lancheria/src/app/modules/checkout/checkout.module.ts
+++ App-Lancheria/src/app/modules/checkout/checkout.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { CheckoutRoutingModule } from './checkout-routing.module';
 import { CartViewComponent } from './components/cart-view/cart-view.component';
 import { CartItemComponent } from './components/cart-item/cart-item.component';
