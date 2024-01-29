530e549ed0aeccd938d949f767d4a9378e199255
“FeReDragon” <f3030ma@gmail.com>
2024-01-29 11:31:15 -0300

    ngif nos links de rotas

--- App-Lancheria/src/app/shared/components/navbar/navbar.component.ts
+++ App-Lancheria/src/app/shared/components/navbar/navbar.component.ts
@@ -1,3 +1,4 @@
+// navbar.component.ts
 import { Component } from '@angular/core';
 import { AuthService } from '../../../core/services/auth.service';
 import { User } from '../../../shared/model/user.model';
@@ -8,7 +9,7 @@ import { User } from '../../../shared/model/user.model';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
-  currentUser: User | null = null; // Atualizado para User | null
+  currentUser: User | null = null;
 
   constructor(private authService: AuthService) {
     this.authService.currentUser.subscribe(x => this.currentUser = x);
@@ -17,4 +18,9 @@ export class NavbarComponent {
   logout() {
     this.authService.logout();
   }
+
+  isAdmin() {
+    return this.currentUser && this.currentUser.tipo === 2;
+  }
 }
+
